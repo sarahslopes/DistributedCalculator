5d553b14aa6871c10e1871dcc7eba5a4633145e7
sarahslopes <sarahslopez25@gmail.com>
2024-05-01 10:32:41 -0300

    Add operations methods

---
+++ src/main/java/org/project/arithmetic/operations/SimpleOperations.java
@@ -0,0 +1,35 @@
+package org.project.arithmetic.operations;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SimpleOperations {
+
+    private static final Integer MAX_NUMBERS =  20;
+
+    public BigDecimal sum(List<BigDecimal> numbers) {
+        return numbers.stream()
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal subtract(List<BigDecimal> numbers) {
+        return numbers.stream()
+                .reduce(BigDecimal::subtract)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    public BigDecimal multiply(List<BigDecimal> numbers) {
+        return numbers.stream()
+                .reduce(BigDecimal.ONE, BigDecimal::multiply);
+    }
+
+    public BigDecimal divide(List<BigDecimal> numbers) {
+        return numbers.stream()
+                .reduce((a, b) -> a.divide(b, BigDecimal.ROUND_HALF_UP))
+                .orElse(BigDecimal.ONE);
+    }
+
+    public boolean isOperationValid(List<BigDecimal> numbersReceived) {
+        return numbersReceived.size() < MAX_NUMBERS;
+    }
+}
